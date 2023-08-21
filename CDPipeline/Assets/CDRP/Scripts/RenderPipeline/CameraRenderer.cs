3ad3948de24160a9cae85d466a79953bdaaf14c2
EssaHuangXu <hxEssa@126.com>
2023-08-22 01:02:10 +0800

    多相机绘制物体、Gizmos

--- CDPipeline/Assets/CDRP/Scripts/RenderPipeline/CameraRenderer.cs
+++ CDPipeline/Assets/CDRP/Scripts/RenderPipeline/CameraRenderer.cs
@@ -1,36 +1,78 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using UnityEngine;
 using UnityEngine.Rendering;
 
 namespace CDPipeline
 {
     public class CameraRenderer : ICameraRenderer
     {
-        private CommandBuffer _buffer = null;
+        private CommandBuffer _buffer = new CommandBuffer()
+		{
+			name = _bufferName,
+		};
 
-        private string _bufferName = "Render Camera";
+        private const string _bufferName = "Render Camera";
 
-        public void Render( ScriptableRenderContext context, Camera camera )
+		private static ShaderTagId _unlitShaderTagId = new ShaderTagId( "SRPDefaultUnlit" );
+
+#if UNITY_EDITOR
+		private static ShaderTagId[] legacyShaderTagIds = {
+	        new ShaderTagId( "Always" ),
+			new ShaderTagId( "ForwardBase" ),
+	        new ShaderTagId( "PrepassBase" ),
+	        new ShaderTagId( "Vertex" ),
+	        new ShaderTagId( "VertexLMRGBM" ),
+	        new ShaderTagId( "VertexLM" )
+        };
+
+		private static Material _errorMaterial = new Material( Shader.Find( "Hidden/InternalErrorShader" ) );
+#endif
+
+		public void Render( ScriptableRenderContext context, Camera camera )
         {
+#if UNITY_EDITOR
+            this.PrepareBuffer( camera );
+
+            this.PrepareForSceneWindow( camera );
+#endif
+
+			if( this.Cull( context, camera, out var cullingResults ) == false )
+            {
+                return;
+            }
+
             this.SetupContext( context, camera );
 
-            this.OnBeginRender( context );
+            this.OnBeginRender( context, camera );
 
-            this.DrawVisibleGeometry(context, camera);
+            this.DrawVisibleGeometry( context, camera, ref cullingResults );
+#if UNITY_EDITOR
+            this.DrawUnsupportedShaders( context, camera, ref cullingResults );
 
+            this.DrawGizmos( context, camera );
+#endif
             this.Submit( context );
         }
 
-        protected void OnBeginRender( ScriptableRenderContext context )
+        protected bool Cull( ScriptableRenderContext context, Camera camera, out CullingResults results )
         {
-            _buffer = new CommandBuffer()
+            if( camera.TryGetCullingParameters( out var parameters ) )
             {
-                name = _bufferName
-            };
+				results = context.Cull( ref parameters );
+                return true;
+            }
+            results = new CullingResults();
+			return false;
+        }
 
-            _buffer.ClearRenderTarget( true, true, Color.clear );
-
-            _buffer.BeginSample( _buffer.name );
-
+        protected void OnBeginRender( ScriptableRenderContext context, Camera camera )
+        {
+			CameraClearFlags flags = camera.clearFlags;
+			_buffer.ClearRenderTarget( flags <= CameraClearFlags.Depth, flags <= CameraClearFlags.Color, flags == CameraClearFlags.Color ? camera.backgroundColor.linear : Color.clear );
+			_buffer.BeginSample( _buffer.name );
             this.ExecuteCommandBuff( context, _buffer );
         }
 
@@ -39,10 +81,23 @@ namespace CDPipeline
             context.SetupCameraProperties( camera );
         }
 
-        protected void DrawVisibleGeometry( ScriptableRenderContext context, Camera camera )
+        protected void DrawVisibleGeometry( ScriptableRenderContext context, Camera camera, ref CullingResults cullingResults )
         {
+            var filterSettings = new FilteringSettings( RenderQueueRange.opaque );
+            var sortingSetting = new SortingSettings( camera )
+            {
+                criteria = SortingCriteria.CommonOpaque
+            };
+			var drawSettings = new DrawingSettings( _unlitShaderTagId, sortingSetting );
+            context.DrawRenderers( cullingResults, ref drawSettings, ref filterSettings );
+            
             context.DrawSkybox( camera );
-        }
+
+            filterSettings.renderQueueRange = RenderQueueRange.transparent;
+            sortingSetting.criteria = SortingCriteria.CommonTransparent;
+            drawSettings.sortingSettings = sortingSetting;
+			context.DrawRenderers( cullingResults, ref drawSettings, ref filterSettings );
+		}
 
         protected void Submit( ScriptableRenderContext context )
         {
@@ -56,5 +111,43 @@ namespace CDPipeline
             context.ExecuteCommandBuffer( buffer );
             buffer.Clear();
         }
-    }
+
+#if UNITY_EDITOR
+        protected void PrepareBuffer( Camera camera )
+        {
+			_buffer.name = camera.name;
+        }
+
+		protected void DrawUnsupportedShaders( ScriptableRenderContext context, Camera camera, ref CullingResults cullingResults )
+        {
+            var drawingSettings = new DrawingSettings( legacyShaderTagIds[0], new SortingSettings( camera ) );
+            drawingSettings.overrideMaterial = _errorMaterial;
+
+			for( var i = 1; i < legacyShaderTagIds.Length; i++ )
+            {
+                drawingSettings.SetShaderPassName( i, legacyShaderTagIds[i] );
+            }
+            
+            var filteringSetting = FilteringSettings.defaultValue;
+            context.DrawRenderers( cullingResults, ref drawingSettings, ref filteringSetting );
+		}
+
+        protected void DrawGizmos( ScriptableRenderContext context, Camera camera )
+        {
+            if( Handles.ShouldRenderGizmos() == true )
+            {
+                context.DrawGizmos( camera, GizmoSubset.PreImageEffects );
+                context.DrawGizmos( camera, GizmoSubset.PostImageEffects );
+            }
+        }
+
+        protected void PrepareForSceneWindow( Camera camera )
+        {
+			if( camera.cameraType == CameraType.SceneView )
+			{
+				ScriptableRenderContext.EmitWorldGeometryForSceneView( camera );
+			}
+		}
+#endif
+	}
 }
